fb40778c0f54ad83e35f2db76413b0d09c8b3867
Sravanthi Yakari <v-syakari@microsoft.com>
2025-04-05 12:42:03 +0530

    Add project files.

---
+++ EmployeeBOApp/Controllers/DeAllocationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeBOApp.Controllers
+{
+    public class DeAllocationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EmployeeBOApp/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(".nav-link").click(function (e) {
+    e.preventDefault();
+    var url = $(this).attr("href");
+
+    $("#main-content").load(url); // This loads the view into the #main-content div
+});
+
